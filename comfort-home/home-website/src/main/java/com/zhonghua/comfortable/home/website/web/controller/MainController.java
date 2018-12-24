172ddeea776a31c2df28abc2d1917ec9a2ba6461
mazeguo <mazeguo@jd.com>
2018-12-25 07:07:40 +0800

    zh-home

---
+++ comfort-home/home-website/src/main/java/com/zhonghua/comfortable/home/website/web/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.zhonghua.comfortable.home.website.web.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author  mazeguo
+ * @date 2018-12-24 22:50
+ * 首页控制类
+ */
+@Controller
+@RequestMapping("/")
+public class MainController {
+    private static final Logger logger = Logger.getLogger(MainController.class);
+
+
+
+}
